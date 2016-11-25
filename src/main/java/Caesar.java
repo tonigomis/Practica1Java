97ee1ac555d9417c57da8641b78e6df5bf1280e2
Toni Gomis <tonigom@hotmail.com>
2016-11-25 02:20:35 +0100

    Practica1 2nd

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,10 +1,27 @@
 
 public class Caesar {
     static String cypher(String s, int delta) {
-        return null;
+        String xifrat = "";
+        s = s.toUpperCase();
+        int punter;
+        for (int i = 0; i < s.length(); i++) {
+            delta = delta % 26;
+            if (s.charAt(i) == 32 || s.charAt(i) < 65 || s.charAt(i) > 90) {
+                xifrat += s.charAt(i);
+            } else if (s.charAt(i) + delta > 'Z') {
+                punter = (s.charAt(i) + delta) - 26;
+                xifrat += (char) punter;
+            } else {
+                xifrat += (char) (s.charAt(i) + delta);
+            }
+        }
+        s = xifrat;
+        return s;
     }
 
     static String decypher(String s, int delta) {
+        String desxifrat = "";
+
         return null;
     }
 
