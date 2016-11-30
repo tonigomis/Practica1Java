40f07b54fac3bed89c1b7929a9184713719cfab4
Toni Gomis <tonigom@hotmail.com>
2016-11-30 17:58:50 +0100

    Practica1 Classe Transposition, tercer commit. Funciona el primer mètode íntegre. Provant el decypher. Passa el primer test, no el segon.

--- src/main/java/Transposition.java
+++ src/main/java/Transposition.java
@@ -1,5 +1,3 @@
-
-
 public class Transposition {
     static String cypher(String s, int dim) {
         String resultat = "";
@@ -24,7 +22,31 @@ public class Transposition {
     }
 
     static String decypher(String s, int dim) {
-        return null;
+        String resultat = "";
+        int col = (int) (Math.ceil(s.length() / (double) dim));
+        //System.out.println(col);
+        //System.out.println(s.length());
+        //System.out.println(dim);
+        char[][] decodifica = new char[dim][col];
+        for (int i = 0, t = 0; i < decodifica.length; i++) {
+            //System.out.println(s.charAt(i));
+            for (int j = 0; j < decodifica[0].length; j++) {
+                //System.out.println(s.charAt(j));
+                if (t == s.length()) {
+                    decodifica[i][j] = '\u0000';
+                    continue;
+                }
+                decodifica[i][j] = s.charAt(t);
+                t++;
+            }
+        }
+        for (int i = 0; i < decodifica[0].length; i++) {
+            for (char[] aCodifica : decodifica) {
+                if (aCodifica[i] != '\u0000') resultat += aCodifica[i];
+            }
+        }
+
+        return resultat;
     }
 
     static String cypher(String s, String key) {
