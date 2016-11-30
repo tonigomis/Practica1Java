40aa3c6f944198a5fc952fb47ed44908d80ab12a
Toni Gomis <tonigom@hotmail.com>
2016-12-01 00:20:35 +0100

    Practica1 Classe Transposition, cinquè commit. Funcionen els dos primers mètodes de xifrar/desxifrar. Canviat nom de variable contador.

--- src/main/java/Transposition.java
+++ src/main/java/Transposition.java
@@ -3,35 +3,34 @@ public class Transposition {
         String resultat = "";
         int fila = (int) (Math.ceil(s.length() / (double) dim));
         char[][] codifica = new char[fila][dim];
-        for (int i = 0, t = 0; i < codifica.length; i++) {
+        for (int i = 0, contador = 0; i < codifica.length; i++) {
             for (int j = 0; j < codifica[i].length; j++) {
-                if (t >= s.length()) {
+                if (contador == s.length()) {
                     codifica[i][j] = '\u0000';
                     continue;
                 }
-                codifica[i][j] = s.charAt(t);
-                t++;
+                codifica[i][j] = s.charAt(contador);
+                contador++;
             }
         }
-        return ompleXifrat(codifica, resultat);
+        return fillCypher(codifica, resultat);
     }
 
     static String decypher(String s, int dim) {
         String resultat = "";
         int fila = (int) (Math.ceil(s.length() / (double) dim));
         char[][] decodifica = new char[fila][dim];
-        int t = 0;
-        for (int i = 0; i < decodifica[0].length; i++) {
+        for (int i = 0, contador = 0; i < decodifica[0].length; i++) {
             for (int j = 0; j < decodifica.length; j++) {
-                if (!(j == decodifica.length - 1 && i > (decodifica[0].length - 1) - (decodifica.length * decodifica[0].length - s.length())) && t < s.length()) {
-                    decodifica[j][i] = s.charAt(t);
-                    t++;
+                if (!(j == decodifica.length - 1 && i > (decodifica[0].length - 1) - (decodifica.length * decodifica[0].length - s.length())) && contador < s.length()) {
+                    decodifica[j][i] = s.charAt(contador);
+                    contador++;
                 } else {
                     decodifica[j][i] = '\u0000';
                 }
             }
         }
-        return ompleDesxifrat(decodifica, resultat);
+        return fillDecypher(decodifica, resultat);
     }
 
     static String cypher(String s, String key) {
@@ -42,7 +41,7 @@ public class Transposition {
         return null;
     }
 
-    static String ompleXifrat(char[][] codifica, String resultat) {
+    static String fillCypher(char[][] codifica, String resultat) {
         for (int i = 0; i < codifica[0].length; i++) {
             for (char[] aCodifica : codifica) {
                 if (aCodifica[i] != '\u0000') resultat += aCodifica[i];
@@ -51,7 +50,7 @@ public class Transposition {
         return resultat;
     }
 
-    static String ompleDesxifrat(char[][] decodifica, String resultat) {
+    static String fillDecypher(char[][] decodifica, String resultat) {
         for (char[] aDecodifica : decodifica) {
             for (int k = 0; k < decodifica[0].length; k++) {
                 if (aDecodifica[k] != '\u0000') {
