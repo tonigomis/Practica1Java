60f6fe863b66ff7024826163136566b175325a67
Toni Gomis <tonigom@hotmail.com>
2016-12-04 03:08:55 +0100

    Practica1 Classe Transposition, setè commit. Funcionen tots els mètodes. Hauria de començar a comentar el codi.

--- src/main/java/Transposition.java
+++ src/main/java/Transposition.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Transposition {
     static String cypher(String s, int dim) {
-        char[][] codifica = cypherToArray(s,dim);
+        char[][] codifica = cypherToArray(s, dim);
         return extractCypher(codifica);
     }
 
@@ -18,77 +18,9 @@ public class Transposition {
     }
 
     static String decypher(String s, String key) {
-        char[][] decodifica = decypherToArray(s, key.length());
+        char[][] decodifica = decypherUnsortedToArray(s, key.length(), key);
         decodifica = Sort(decodifica, key);
         return extractDecypher(decodifica);
-    }
-
-    static char[][] Sort(char[][] decodifica, String key) {
-        char[] ordenat = key.toCharArray();
-        Arrays.sort(ordenat);
-        int[] ordre = new int[decodifica[0].length];
-        char[][] desxifrat = new char[decodifica.length][decodifica[0].length];
-        int contador;
-        for (int i = 0; i <= desxifrat[0].length-1; i++) {
-            for (contador = 0; contador < key.length(); contador++) {
-                if (key.charAt(i) == ordenat[contador]) {
-                    ordre[i] = contador;
-                    ordenat[contador] = 0;
-                    break;
-                }
-            }
-        }
-        for (int x = 0; x < decodifica[0].length; x++) {
-            for (int y = 0; y < decodifica.length; y++) {
-                desxifrat[y][x] = decodifica[y][ordre[x]];
-            }
-        }
-        return desxifrat;
-    }
-
-    static char[][] unSort(char[][] codifica, String key) {
-        char[] desordenat = key.toCharArray();
-        Arrays.sort(desordenat);
-        int[] ordre = new int[codifica[0].length];
-        char[][] xifrat = new char[codifica.length][codifica[0].length];
-        for (int i = 0; i < xifrat[0].length; i++) {
-            for (int contador = 0; contador < key.length(); contador++) {
-                if (key.charAt(i) == desordenat[contador]) {
-                    ordre[i] = contador;
-                    desordenat[contador] = 0;
-                    break;
-                }
-            }
-        }
-        for (int x = 0, contador = 0; x < codifica[0].length; x++) {
-            for (int y = 0; y < codifica.length; y++) {
-                xifrat[y][ordre[contador]] = codifica[y][x];
-            }
-            contador++;
-        }
-        return xifrat;
-    }
-
-    static String extractCypher(char[][] codifica) {
-        String resultat = "";
-        for (int i = 0; i < codifica[0].length; i++) {
-            for (char[] aCodifica : codifica) {
-                if (aCodifica[i] != '\u0000') resultat += aCodifica[i];
-            }
-        }
-        return resultat;
-    }
-
-    static String extractDecypher(char[][] decodifica) {
-        String resultat = "";
-        for (char[] aDecodifica : decodifica) {
-            for (int k = 0; k < decodifica[0].length; k++) {
-                if (aDecodifica[k] != '\u0000') {
-                    resultat += aDecodifica[k];
-                }
-            }
-        }
-        return resultat;
     }
 
     static char[][] cypherToArray(String s, int dim) {
@@ -121,5 +53,105 @@ public class Transposition {
             }
         }
         return decodifica;
+    }
+
+    static char[][] decypherUnsortedToArray(String s, int dim, String key) {
+        char[] clau = key.toCharArray();
+        Arrays.sort(clau);
+        int[] ordre = new int[key.length()];
+        int[] ordreInicial = new int[key.length()];
+        int contador;
+        for (int i = 0; i < key.length(); i++) {
+            for (contador = 0; contador < key.length(); contador++) {
+                if (key.charAt(i) == clau[contador]) {
+                    ordre[i] = contador;
+                    clau[contador] = 0;
+                    ordreInicial[contador] = i;
+                    break;
+                }
+            }
+        }
+        int fila = (int) (Math.ceil(s.length() / (double) dim));
+        char[][] decodifica = new char[fila][dim];
+        int posicio = 0;
+        for (int x = 0; x < decodifica[0].length; x++) {
+            for (int y = 0; y < decodifica.length; y++) {
+                if (!((y == fila - 1) && (ordreInicial[x] >= key.length() - (dim * fila - s.length())))) {
+                    decodifica[y][x] = s.charAt(posicio);
+                    posicio++;
+                } else {
+                    decodifica[y][x] = 0;
+                }
+            }
+        }
+        return decodifica;
+    }
+
+    static char[][] unSort(char[][] codifica, String key) {
+        char[] desordenat = key.toCharArray();
+        Arrays.sort(desordenat);
+        int[] ordre = new int[codifica[0].length];
+        char[][] xifrat = new char[codifica.length][codifica[0].length];
+        for (int i = 0; i < xifrat[0].length; i++) {
+            for (int contador = 0; contador < key.length(); contador++) {
+                if (key.charAt(i) == desordenat[contador]) {
+                    ordre[i] = contador;
+                    desordenat[contador] = 0;
+                    break;
+                }
+            }
+        }
+        for (int x = 0, contador = 0; x < codifica[0].length; x++) {
+            for (int y = 0; y < codifica.length; y++) {
+                xifrat[y][ordre[contador]] = codifica[y][x];
+            }
+            contador++;
+        }
+        return xifrat;
+    }
+
+    static char[][] Sort(char[][] decodifica, String key) {
+        char[] ordenat = key.toCharArray();
+        Arrays.sort(ordenat);
+        int[] ordre = new int[decodifica[0].length];
+        char[][] desxifrat = new char[decodifica.length][decodifica[0].length];
+        int contador;
+        for (int i = 0; i <= desxifrat[0].length - 1; i++) {
+            for (contador = 0; contador < key.length(); contador++) {
+                if (key.charAt(i) == ordenat[contador]) {
+                    ordre[i] = contador;
+                    ordenat[contador] = 0;
+                    break;
+                }
+            }
+        }
+        for (int x = 0; x < decodifica[0].length; x++) {
+            for (int y = 0; y < decodifica.length; y++) {
+                desxifrat[y][x] = decodifica[y][ordre[x]];
+            }
+        }
+        return desxifrat;
+    }
+
+    static String extractCypher(char[][] codifica) {
+        String resultat = "";
+        for (int i = 0; i < codifica[0].length; i++) {
+            for (char[] aCodifica : codifica) {
+                if (aCodifica[i] != '\u0000') resultat += aCodifica[i];
+            }
+        }
+        return resultat;
+    }
+
+    static String extractDecypher(char[][] decodifica) {
+        String resultat = "";
+        for (char[] aDecodifica : decodifica) {
+            for (int k = 0; k < decodifica[0].length; k++) {
+                if (aDecodifica[k] != '\u0000') {
+                    resultat += aDecodifica[k];
+                }
+            }
+        }
+        return resultat;
     }
 }
