4850ea1b930b9fa654ef60c19cfd2b0d264402d2
Toni Gomis <tonigom@hotmail.com>
2016-12-04 23:31:54 +0100

    Practica1 Classe Caesar. Redissenyada la funció magic que endevina la clau del xifrat.

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,7 +1,6 @@
 
 public class Caesar {
     static int alfa = 26;
-
     static String cypher(String s, int delta) {
         String xifrat = "";
         s = s.toUpperCase();
@@ -40,7 +39,8 @@ public class Caesar {
         return desxifrat;
     }
 
-    static String magic(String s) {
+    /* Aquesta era la versió original de la funció magic abans d'intentar-la amb un càlcul estadístic d'ocurrències */
+/*    static String magic(String s) {
         int delta = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ' ') {
@@ -55,5 +55,66 @@ public class Caesar {
             }
         }
         return decypher(s, delta);
+    }*/
+
+    static String magic(String s) {
+        String st = normalitza(s);
+        int valorTemporal = findMax(st);
+        int delta = guessDelta(valorTemporal);
+        return decypher(s, delta);
+    }
+
+    static char findMax(String s) {
+        char findMax;
+        int[] ocurrencies = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            ocurrencies[s.charAt(i) - 'A']++;
+        }
+        int num = -1;
+        int major = ocurrencies[0];
+        for (int j = 0; j < ocurrencies.length; j++) {
+            if (ocurrencies[j] > major) {
+                major = ocurrencies[j];
+                num = j + 'A';
+            }
+        }
+        findMax = (char) num;
+        return findMax;
+    }
+
+    static int guessDelta(int valorTemporal) {
+        int delta = valorTemporal - 'E';
+        if (delta < 0) delta += alfa;
+        return delta;
+    }
+
+    static String normalitza(String s) {
+        char caracter;
+        String normalitzat = "";
+        for (int i = 0; i < s.length(); i++) {
+            caracter = s.charAt(i);
+            if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
+                normalitzat += caracter;
+                continue;
+            } else if (s.charAt(i) == ' ') {
+                continue;
+            } else if (s.charAt(i) >= 97 && s.charAt(i) <= 122) {
+                caracter -= 32;
+            } else if (s.charAt(i) >= 224 && s.charAt(i) <= 230 || s.charAt(i) >= 192 && s.charAt(i) <= 198) {
+                caracter = 'A';
+            } else if (s.charAt(i) >= 232 && s.charAt(i) <= 235 || s.charAt(i) >= 200 && s.charAt(i) <= 203) {
+                caracter = 'E';
+            } else if (s.charAt(i) >= 236 && s.charAt(i) <= 239 || s.charAt(i) >= 204 && s.charAt(i) <= 207) {
+                caracter = 'I';
+            } else if (s.charAt(i) >= 242 && s.charAt(i) <= 246 || s.charAt(i) >= 210 && s.charAt(i) <= 214) {
+                caracter = 'O';
+            } else if (s.charAt(i) >= 249 && s.charAt(i) <= 252 || s.charAt(i) >= 217 && s.charAt(i) <= 220) {
+                caracter = 'U';
+            } else {
+                continue;
+            }
+            normalitzat += caracter;
+        }
+        return normalitzat;
     }
 }
